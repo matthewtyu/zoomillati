5db549170cc83339e65b70be2fd982d5ac8a6ee2
Jenny Wong <jew007@ucsd.edu>
2015-10-03 11:52:36 -0700

    Initial Commit

---
+++ zoominati/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zoominati
+//
+//  Created by Jenny Wong on 10/3/15.
+//  Copyright (c) 2015 shdacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
